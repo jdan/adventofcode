ee2077f5515bfc59de99e35ff070d9fe73cfd81b
Jordan Scales <scalesjordan@gmail.com>
2015-12-17 13:18:16 -0500

    advent of code day 3

---
+++ 3a-present-delivery.rb
@@ -0,0 +1,21 @@
+presents = Hash.new(0)
+position = [0, 0]
+
+presents[position.join(",")] += 1
+
+gets.chomp.each_char do |direction|
+    case direction
+    when ">"
+        position[0] += 1
+    when "<"
+        position[0] -= 1
+    when "^"
+        position[1] -= 1
+    when "v"
+        position[1] += 1
+    end
+
+    presents[position.join(",")] += 1
+end
+
+puts presents.length
---
+++ 3b-present-delivery.rb
@@ -0,0 +1,29 @@
+presents = Hash.new(0)
+
+santa_position = [0, 0]
+robo_position = [0, 0]
+
+presents["0,0"] += 1
+
+gets.chomp.split("").each_with_index do |direction, i|
+    if i.even?
+        position = santa_position
+    else
+        position = robo_position
+    end
+
+    case direction
+    when ">"
+        position[0] += 1
+    when "<"
+        position[0] -= 1
+    when "^"
+        position[1] -= 1
+    when "v"
+        position[1] += 1
+    end
+
+    presents[position.join(",")] += 1
+end
+
+puts presents.length
