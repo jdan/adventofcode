58f60073502c6d3e5fc6356c1ff97edd6748da18
Jordan Scales <scalesjordan@gmail.com>
2015-12-17 13:18:56 -0500

    advent of code day 9

---
+++ 9a-hamiltonian-path.rb
@@ -0,0 +1,28 @@
+require "set"
+
+graph = Hash.new
+cities = Set.new
+
+ARGF.each do |line|
+    a, b, dist = line.scan(/(\w+) to (\w+) = (\w+)/)[0]
+
+    cities.add(a)
+    cities.add(b)
+
+    graph[a] ||= Hash.new(Float::INFINITY)
+    graph[b] ||= Hash.new(Float::INFINITY)
+
+    graph[a][b] = dist.to_i
+    graph[b][a] = dist.to_i
+end
+
+def path_distance(graph, path)
+    distance = 0
+    path.each_cons(2) do |a, b|
+        graph[a] ||= Hash.new(Float::INFINITY)
+        distance += graph[a][b]
+    end
+    distance
+end
+
+puts cities.to_a.permutation.map { |path| path_distance(graph, path) }.min
