3464ae146ef99b11edae487c3dd47fd8ae9ca7ab
Jordan Scales <scalesjordan@gmail.com>
2015-12-17 13:18:42 -0500

    advent of code day 6

---
+++ 6a-christmas-lights.rb
@@ -0,0 +1,43 @@
+lights = Array.new(1000) {
+    Array.new(1000, false)
+}
+
+def over_square(a, b, x, y, &block)
+    a.upto(x) do |i|
+        b.upto(y) do |j|
+            block.call(i, j)
+        end
+    end
+end
+
+ARGF.each do |line|
+    scan = line.match /(.+) (\d+),(\d+) through (\d+),(\d+)/
+
+    command = scan[1]
+    a, b = scan[2].to_i, scan[3].to_i
+    x, y = scan[4].to_i, scan[5].to_i
+
+    case command
+    when "turn on"
+        over_square(a, b, x, y) do |i, j|
+            lights[i][j] = true
+        end
+    when "turn off"
+        over_square(a, b, x, y) do |i, j|
+            lights[i][j] = false
+        end
+    when "toggle"
+        over_square(a, b, x, y) do |i, j|
+            lights[i][j] = !lights[i][j]
+        end
+    end
+end
+
+total = 0
+over_square(0, 0, 999, 999) do |i, j|
+    if lights[i][j]
+        total += 1
+    end
+end
+
+puts total
---
+++ 6b-christmas-lights.rb
@@ -0,0 +1,45 @@
+lights = Array.new(1000) {
+    Array.new(1000, 0)
+}
+
+def over_square(a, b, x, y, &block)
+    a.upto(x) do |i|
+        b.upto(y) do |j|
+            block.call(i, j)
+        end
+    end
+end
+
+ARGF.each do |line|
+    scan = line.match /(.+) (\d+),(\d+) through (\d+),(\d+)/
+
+    command = scan[1]
+    a, b = scan[2].to_i, scan[3].to_i
+    x, y = scan[4].to_i, scan[5].to_i
+
+    case command
+    when "turn on"
+        over_square(a, b, x, y) do |i, j|
+            lights[i][j] += 1
+        end
+    when "turn off"
+        over_square(a, b, x, y) do |i, j|
+            lights[i][j] -= 1
+
+            if lights[i][j] < 0
+                lights[i][j] = 0
+            end
+        end
+    when "toggle"
+        over_square(a, b, x, y) do |i, j|
+            lights[i][j] += 2
+        end
+    end
+end
+
+total = 0
+over_square(0, 0, 999, 999) do |i, j|
+    total += lights[i][j]
+end
+
+puts total
