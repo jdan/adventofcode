20bcb02c1d12e11caebdceb3088698a22f8167cc
Jordan Scales <scalesjordan@gmail.com>
2016-12-15 21:29:51 -0500

    2016 day 6

---
+++ 2016/06a-repitition.rb
@@ -0,0 +1,16 @@
+# http://adventofcode.com/2016/day/6
+
+messages = ARGF.to_a
+
+puts 0.upto(messages.first.chomp.length - 1)    # for each char index `n`
+    .to_a
+    .map { |i|
+        messages
+            .map { |message| message[i] }       # get the `n`th characters
+            .group_by { |a| a }                 # group by letter
+            .to_a
+            .sort_by { |pair| -pair[1].count }  # sort by frequency
+            .first                              # extract the most frequent
+            .first                              # extract the character
+    }
+    .join("")                                   # join 'em back
