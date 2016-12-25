bf93ff37d7fc0bbf03027c4a101c1221ff416737
Jordan Scales <scalesjordan@gmail.com>
2016-12-25 15:44:06 -0500

    huh didn't know I could have instance variables like that

--- 2016/10a-microchips.rb
+++ 2016/10a-microchips.rb
@@ -1,8 +1,6 @@
 # http://adventofcode.com/2016/day/10
-#
-# TODO(jordan): These globals should just be static variables for some class
 
-state = {
+@state = {
     bots: Hash.new { |hash, key|
         hash[key] = { low: nil, high: nil }
     },
@@ -11,10 +9,10 @@ state = {
     },
 }
 
-listeners = {}
+@listeners = {}
 
-def give_to_bot(state, listeners, bot, value, reactive = false)
-    bot_state = state[:bots][bot]
+def give_to_bot(bot, value, reactive = false)
+    bot_state = @state[:bots][bot]
 
     if bot_state[:low] == nil && bot_state[:high] == nil
         bot_state[:low] = value
@@ -37,7 +35,7 @@ def give_to_bot(state, listeners, bot, value, reactive = false)
     end
 
     if reactive && bot_state[:low] && bot_state[:high]
-        listeners[bot].()
+        @listeners[bot].()
     end
 
     # This is where we check what the bot is comparing
@@ -46,47 +44,47 @@ def give_to_bot(state, listeners, bot, value, reactive = false)
         exit
     end
 
-    state[:bots][bot] = bot_state
+    @state[:bots][bot] = bot_state
 end
 
-def give_to_output(state, output, value)
-    state[:outputs][output] = value
+def give_to_output(output, value)
+    @state[:outputs][output] = value
 end
 
-def set_bot_listener(listeners, state, bot, low_type, low_dest, high_type, high_dest)
-    listeners[bot] = ->() {
-        bot_state = state[:bots][bot]
+def set_bot_listener(bot, low_type, low_dest, high_type, high_dest)
+    @listeners[bot] = ->() {
+        bot_state = @state[:bots][bot]
 
         if low_type == "output"
-            give_to_output(state, low_dest, bot_state[:low])
+            give_to_output(low_dest, bot_state[:low])
         else
-            give_to_bot(state, listeners, low_dest, bot_state[:low], true)
+            give_to_bot(low_dest, bot_state[:low], true)
         end
 
         if high_type == "output"
-            give_to_output(state, high_dest, bot_state[:high])
+            give_to_output(high_dest, bot_state[:high])
         else
-            give_to_bot(state, listeners, high_dest, bot_state[:high], true)
+            give_to_bot(high_dest, bot_state[:high], true)
         end
     }
 end
 
-def read_instruction(state, listeners, instruction)
+def read_instruction(instruction)
     case instruction
     when /value (\d+) goes to bot (\d+)/
-        give_to_bot(state, listeners, $2.to_i, $1.to_i)
+        give_to_bot($2.to_i, $1.to_i)
     when /bot (\d+) gives low to (\w+) (\d+) and high to (\w+) (\d+)/
-        set_bot_listener(listeners, state, $1.to_i, $2, $3.to_i, $4, $5.to_i)
+        set_bot_listener($1.to_i, $2, $3.to_i, $4, $5.to_i)
     end
 end
 
 ARGF.each do |instruction|
-    read_instruction(state, listeners, instruction)
+    read_instruction(instruction)
 end
 
-listeners.keys.each do |bot|
-    bot_state = state[:bots][bot]
+@listeners.keys.each do |bot|
+    bot_state = @state[:bots][bot]
     if bot_state[:low] && bot_state[:high]
-        listeners[bot].()
+        @listeners[bot].()
     end
 end
