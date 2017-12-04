023fe6bae1464a712f58f11ab359a15a1fb6b1ad
Jordan Scales <scalesjordan@gmail.com>
2017-12-03 18:23:58 -0800

    iterative process

--- clj/src/adventofcode/2017/03_spiral.clj
+++ clj/src/adventofcode/2017/03_spiral.clj
@@ -1,50 +1,85 @@
 (ns adventofcode.2017.03-spiral)
 
-(defn biggest-odd-sqrt
-  "Finds the largest odd number whose square is less than n."
-  [n]
-  (let [sqrt-candidate (int (Math/sqrt n))]
-    (if (odd? sqrt-candidate)
-      sqrt-candidate
-      (dec sqrt-candidate))))
+(defn next-pos
+  "Gets the next pos"
+  [pos dir]
+  (let [[x y] pos]
+    (cond
+      (= dir :up) [x (inc y)]
+      (= dir :left) [(dec x) y]
+      (= dir :down) [x (dec y)]
+      (= dir :right) [(inc x) y])))
+
+(defn next-dir
+  "Gets the next-direction"
+  [dir left-before-turn]
+  (if (= left-before-turn 0)
+    ({:up :left
+      :left :down
+      :down :right
+      :right :up} dir)
+    dir))
+
+(defn next-travel-length
+  "Gets the next travel length"
+  [direction left-before-turn travel-length]
+  (if (> left-before-turn 0)
+    travel-length
+    (if (or (= :left direction)
+            (= :right direction))
+      (inc travel-length)
+      travel-length)))
+
+(defn next-left-before-turn
+  "Gets the next left-before-turn"
+  [direction left-before-turn travel-length]
+  (if (> left-before-turn 0)
+    (dec left-before-turn)
+    (next-travel-length direction left-before-turn travel-length)))
+
+(defn run-spiral
+  "Runs a spiral"
+  [args]
+  (let [{:keys [board pos last-value direction
+                travel-length left-before-turn
+                get-square-value is-done]} args
+        new-value (get-square-value last-value board pos)
+        new-board (assoc board pos new-value)
+        new-dir (next-dir direction left-before-turn)
+        new-pos (next-pos pos new-dir)
+        new-travel-length (next-travel-length
+                           new-dir left-before-turn travel-length)
+        new-left-before-turn (next-left-before-turn
+                              new-dir left-before-turn travel-length)]
+    (if (is-done new-value)
+      [new-value board pos]
+      (recur
+       {:board new-board
+        :pos new-pos
+        :last-value new-value
+        :direction new-dir
+        :travel-length new-travel-length
+        :left-before-turn new-left-before-turn
+        :get-square-value get-square-value
+        :is-done is-done}))))
 
 (defn distance
-  "Finds the taxicab distance from (x, y) to the origin"
+  "Computes taxicab distance"
   [x y]
   (+ (Math/abs x) (Math/abs y)))
 
 (defn run-2017-03a
   [lines]
-  (let [input (Integer. (first lines))
-        start (biggest-odd-sqrt input)
-        x (quot start 2)
-        y (* -1 x)
-
-        length (inc start)
-        ; Corner values
-        bottom-right (* start start)
-        top-right (+ bottom-right length)
-        top-left (+ top-right length)
-        bottom-left (+ top-left length)
-        next-bottom-right (+ bottom-left length)]
-    (cond
-      (= bottom-right input) (distance x y)
-      ; Right side
-      (<= input top-right)
-      (distance (inc x)
-                (+ y (- input bottom-right 1)))
-
-      ; Top side
-      (<= input top-left)
-      (distance (- x (- input top-right 1))
-                (+ y (- length 1)))
-
-      ; Left side
-      (<= input bottom-left)
-      (distance (- x length)
-                (+ y (- bottom-left input)))
-
-      ; Bottom side
-      :else
-      (distance (- (inc x) (- next-bottom-right input))
-                (dec y)))))
+  (let
+   [input (Integer. (first lines))
+    [_ _ [x y]]
+    (run-spiral
+     {:board {[0 0] 1}
+      :pos [1 0]
+      :last-value 1
+      :direction :right
+      :travel-length 0
+      :left-before-turn 0
+      :get-square-value (fn [last-value _ _] (inc last-value))
+      :is-done #(= input %1)})]
+    (distance x y)))
