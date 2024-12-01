d1d9358e4b8f675dd5789a185eaded9f2d4f98f5
Jordan Scales <scalesjordan@gmail.com>
2024-12-01 10:27:43 -0500

    use .tally

--- 2024/01b-historian-hysteria.rb
+++ 2024/01b-historian-hysteria.rb
@@ -3,6 +3,6 @@ left, right = $stdin
   .map { |line| line.chomp.split(/\s+/).map(&:to_i) }
   .transpose
 
-counts = right.group_by(&:itself).transform_values(&:count)
+counts = right.tally
 
 p left.sum { |l| l * (counts[l] || 0) }
